3730ee2de30a70b699f73ef689fc17dd07148add
maxmraz <max.mraz.11@gmail.com>
2018-10-26 21:31:38 -0400

    added carousel in projects

--- js/project-slideshow.js
+++ js/project-slideshow.js
@@ -1,24 +1,23 @@
-const slides = document.querySelectAll('.mySlides')
-const dots = document.querySelectAll('.dot')
-
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
-	showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
-	showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
-function showSlides(n, slides, dots) {
-	let i;
-	if (n > slides.length){slideIndex = 1}
-	if (n < 1) {slideIndex = slides.length}
-	for (i=0; 1 < dots.length; i++){
-		dots[i].className = dots[i].className.replace('active', '');
-	}
-	slides[slideIndex-1].style.display = 'block';
-	dots[slideIndex-1].className += 'active';
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block"; 
 }
